4eab9cbc3af016eb721194a615578f81ed118041
tiago simao <tiagosimaorodri123@gmail.com>
2025-11-25 02:03:20 -0300

    criado o pedido service e criado o metodo de salvar os itens do pedido, o metodo de salvar pedido e atualizar o item pedido definindo o pedido ao qual ele faz parte, por conta do uso do UUID

---
+++ processador/src/main/java/br/com/pedidos/processador/service/PedidoService.java
@@ -0,0 +1,48 @@
+package br.com.pedidos.processador.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import br.com.pedidos.processador.entity.ItemPedido;
+import br.com.pedidos.processador.entity.Pedido;
+import br.com.pedidos.processador.repository.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	private final Logger logger = LoggerFactory.getLogger(PedidoService.class);
+	private final PedidoRepository pedidoRepository;
+	private final ProdutoService produtoService;
+	private final ItemPedidoService itemPedidoService;
+	 
+	 
+	
+	public PedidoService(PedidoRepository pedidoRepository, ProdutoService produtoService, ItemPedidoService itemPedidoService) {
+		this.pedidoRepository = pedidoRepository;
+		this.produtoService = produtoService;
+		this.itemPedidoService = itemPedidoService;
+	}
+	
+	public void save(Pedido pedido) {
+		//salvamos os produtos
+		produtoService.save(pedido.getItens());
+		
+		// salvamos os itens do  pedido
+		List<ItemPedido> itemPedidos = itemPedidoService.save(pedido.getItens());
+		
+		//salvamos o pedido
+		pedidoRepository.save(pedido);
+		
+		// atualiza o item pedido definindo o pedido ao qual ele faz parte. 
+		itemPedidoService.updatedItemPedido(itemPedidos, pedido);
+		
+		
+		logger.info("Pedido salvo: {}", pedido.toString());
+	}
+	
+	
+}
+
